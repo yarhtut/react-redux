ece409f003b0f32d0ad885d99ac294b594f9104a
yarhtut <yar@boost.co.nz>
2016-03-29 23:50:08 +1300

    add child routes

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Component } from 'react';
 
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+      	React simple starter
+      	{this.props.children}
+      </div>
     );
   }
 }
