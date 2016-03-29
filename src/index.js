5ec0b64964c2fdd265fc518e7e4bf94f92e051a5
yarhtut <yar@boost.co.nz>
2016-03-29 23:41:35 +1300

    add routes

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 
 import reducers from './reducers';
+import routes from './routes';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} />
+    <Router history={browserHistory} routes={routes}/>
   </Provider>
   , document.querySelector('.container'));
