99a2a09d2773d691e2520670ea9be8f389d8b68a
yarhtut <yar@boost.co.nz>
2016-03-31 23:17:39 +1300

    add posts index components

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actons/index';
 
-export default () => {
-	return <div> List of blog posts. </div>;
+
+class PostsIndex extends Component {
+
+	componentWillMoount() {
+		this.props.fetchPosts();
+		console.log('this will render first time ');
+	}
+
+	render() {
+		return (
+			<div> List of blog Posts</div>
+		);
+	}
 }
+
+// function mapDispatchToProps(dispatch) {
+// 	return bindActionCreators({ fetchPosts }, dispatch);
+// }
+
+export default connect (null, { fetchPosts: fetchPosts }) (PostsIndex);
