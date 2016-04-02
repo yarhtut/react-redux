33af9137350c0d0526b649b8eb8edddc1958847d
yarhtut <yar@boost.co.nz>
2016-04-02 23:03:42 +1300

    create Link to post new page

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 
 class PostsIndex extends Component {
@@ -13,7 +14,14 @@ class PostsIndex extends Component {
 
 	render() {
 		return (
-			<div> List of blog Posts</div>
+			<div className="container"> 
+				<div className="text-xs-right">
+					<Link to="/posts/new" className="btn btn-primary">
+						Create New Post
+					</Link>
+				</div>
+			List of blog Posts
+			</div>
 		);
 	}
 }
