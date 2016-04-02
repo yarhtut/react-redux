33d39258d01384cebb5eaa462bcc95e2f6cc5898
yarhtut <yar@boost.co.nz>
2016-04-02 23:30:15 +1300

    add combine reducer with formReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import PostsReducer fomr './reducer_posts';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
