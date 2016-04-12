b4a34ad691d1ffce5b5d97baf0ca593771486763
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-04-12 21:18:37 +1200

    default props

--- Example/public/app.jsx
+++ Example/public/app.jsx
@@ -1,4 +1,28 @@
+var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'Default messaage'
+    }
+  },
+
+  render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>!!!Hello {name}!!!</h1>
+        <p>!!{message} !!</p>
+      </div>
+    )
+  }
+});
+
+var firstName = "Yar";
+var message ="This is from React";
+
 ReactDOM.render(
-  <h1>Hello React!</h1>,
+  <Greeter name={firstName} message={message} />,
   document.getElementById('app')
 );
