08169d04ce5c704490c3d9b78fa38476da7008eb
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-04-12 22:01:46 +1200

    child components and pass props and state

--- Example/public/app.jsx
+++ Example/public/app.jsx
@@ -1,46 +1,71 @@
+var GreeterMessage = React.createClass({
+  render:function () {
+    var name = this.props.name;
+    var message =this.props.message;
+
+    return (
+      <div>
+        <h1>Hello {name} !!</h1>
+        <p>This is paragraph {message}</p>
+      </div>
+    )
+  }
+});
+
+var GreeterForm = React.createClass({
+  onFromSubmit: function(e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+    if(name.length > 0 ) {
+      this.refs.name.value = '';
+
+      this.props.onNewName(name);
+    }
+  },
+
+  render: function () {
+    return(
+      <form onSubmit={this.onFromSubmit}>
+        <input type="text" ref="name" />
+        <button>Set Name</button>
+      </form>
+    )
+  }
+})
+
 var Greeter = React.createClass({
-  getDefaultProps: function() {
+  getDefaultProps: function () {
     return {
       name: 'React',
       message: 'Default messaage'
     }
   },
 
-  getInitialState: function() {
+  getInitialState: function () {
     return {
       name: this.props.name
     }
   },
 
-  onButtonClick: function(e) {
-    e.preventDefault();
-
-    var nameRef = this.refs.name;
-
-    var name = nameRef;
-    nameRef = '';
-
-    if(typeof name === 'string' && name.length > 0) {
-      this.setState({
-        name: name
-      });
-    }
+  handleNewName: function (name) {
+    this.setState({
+      name: name
+    })
   },
 
   // render
-  render: function() {
+  render: function () {
     var name = this.state.name;
     var message = this.props.message;
 
     return (
       <div>
-        <h1>!!!Hello {name}!!!</h1>
-        <p>!!{message} !!</p>
 
-        <form onSubmit={this.onButtonClick}>
-          <input type="text" ref="name" />
-          <button>Set Name</button>
-        </form>
+        <GreeterMessage name={name} message ={message}/>
+
+        <GreeterForm onNewName = {this.handleNewName}/>
+
       </div>
     )
   }// render end
