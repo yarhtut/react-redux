378d00097de145b3ea23cad64bb5b53e12c1f5ff
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-04-14 20:20:28 +1200

    create App/componets and add NavigatorIOs

--- Native/github/index.ios.js
+++ Native/github/index.ios.js
@@ -8,45 +8,33 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS,
 } from 'react-native';
 
-class github extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var Main = require('./App/Components/Main');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#111111'
+  }
 });
+
+class github extends Component {
+  render() {
+    return (
+    <NavigatorIOS
+      style={styles.container}
+      initialRoute={{
+        title: 'Github Notetaker',
+        component: Main
+      }}
+      />
+    );
+  }
+}
+
+
 
 AppRegistry.registerComponent('github', () => github);
