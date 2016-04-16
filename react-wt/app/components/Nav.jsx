12a7d3c15403a02aa7b6e7d01e0d7777a70e34fc
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-04-16 14:04:32 +1200

    add indexLink from react router

--- react-wt/app/components/Nav.jsx
+++ react-wt/app/components/Nav.jsx
@@ -1,9 +1,15 @@
 var React = require('react');
+var { Link, IndexLink } = require('react-router');
 
 var Nav = React.createClass({
   render: function () {
     return (
-      <h2>Nav Component</h2>
+      <div>
+        <h2>Nav Component</h2>
+        <IndexLink to='/' activeClassName="active" activeStyle={{fontWeight: 'bold'}}> Get Weather</IndexLink>
+        <Link to='/about' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+        <Link to='/examples' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Example</Link>
+      </div>
     );
   }
 });
