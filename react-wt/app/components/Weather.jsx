af4274734e3a1e787d49a78e5946cfa62db12f0e
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-04-16 17:46:04 +1200

    add weather child component

--- react-wt/app/components/Weather.jsx
+++ react-wt/app/components/Weather.jsx
@@ -1,9 +1,32 @@
 var React = require('react');
+var WeatherForm = require('WeatherForm');
+var WeatherMessage = require('WeatherMessage');
+
 
 var Weather = React.createClass({
+  //getInitialState
+  getInitialState: function () {
+    return {
+      location: 'Miami',
+      temp: 88
+    }
+  },
+  // handleSearch
+  handleSearch: function (location) {
+    this.setState({
+      location: location,
+      temp: 23
+    });
+  },
+  // render
   render: function() {
+    var {temp, location} = this.state;
     return (
-      <h3> Weather Component</h3>
+      <div>
+        <h3> Weather Component</h3>
+        <WeatherForm onSearch={this.handleSearch}/>
+        <WeatherMessage temp={temp} location={location} />
+      </div>
     )
   }
 });
