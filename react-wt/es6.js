eececd7183b760ae2abc56cfcf59cab7148892ef
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-04-16 20:32:29 +1200

    arrow funtion example

---
+++ react-wt/es6.js
@@ -0,0 +1,28 @@
+// var names = ['yar', 'luke', 'maen', 'michael'];
+// //
+// // names.forEach(function (name) {
+// //   console.log('forEach',name);
+// // })
+// //
+// // names.forEach((name) => {
+// //   console.log('forarrowEach',name)
+// // })
+// //
+// //
+// // names.forEach((name) => console.log(name))
+//
+// // var returnMe = (name) => name + '!!';
+// //
+// // console.log(returnMe('Yar'));
+//
+//
+// var person = {
+//   name: 'Yar',
+//   greet: function () {
+//     names.forEach( (name) =>{
+//       console.log(this.name + 'say hi to '+ name)
+//     })
+//   }
+// }
+//
+// person.greet();
