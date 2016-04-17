2effb5cbbe61392be2f3f9d3560e6d17e72035f2
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-04-17 22:07:34 +1200

    add css foundation script loader

--- react-wt/app/components/About.jsx
+++ react-wt/app/components/About.jsx
@@ -1,11 +1,18 @@
 var React = require('react');
 
-var About = React.createClass({
-  render: function() {
-    return (
-      <h3> About Component</h3>
-    )
-  }
-});
+// var About = React.createClass({
+//   render: function() {
+//     return (
+//       <h3> About Component</h3>
+//     )
+//   }
+// });
+
+// stateless Component
+const About = function (props) {
+  return (
+    <h3> About Component</h3>
+  )
+};
 
 module.exports = About;
