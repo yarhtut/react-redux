7e0ec359af013f319a352a1bcba3d854fda307ef
yarhtut <yar@boost.co.nz>
2016-06-17 11:16:53 +1200

    querystirng parameters

--- egghead_1/src/App.js
+++ egghead_1/src/App.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import { Router , Route, Link, IndexRoute, browserHistory } from 'react-router';
 
+const Page = (props) =>
+    <div><h1>{props.location.query.message || 'Hello'}</h1><Links /></div>
 
-const Home = () => <h1> Home</h1>
-const HomeBody = () => <div>This is the home body</div>
-const Other = () => <h1> Other</h1>
-const OtherBody = () => <div>This is the other body</div>
-
-const Links= () =>
+const Links = () =>
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/other">Other</Link>
+        <Link to={{ pathname: '/', query: { message: 'Yo'}} }>Yo</Link>
     </nav>
-
-const Container = (props) =>
-    <div> {props.header}{props.body}<Links /></div>
 
 class App extends React.Component {
     render() {
         return (
-            <Router history = { browserHistory }>
-                <Route path="/" component={Container}>
-                    <IndexRoute components={{ header: Home, body: HomeBody}}></IndexRoute>
-                    <Route path="/other" components={{ header: Other, body: OtherBody}}></Route>
-                </Route>
+            <Router histroy = { browserHistory }>
+                <Route path="/" component={Page}></Route>
             </Router>
-        );
+        )
     }
 }
-
 export default App;
