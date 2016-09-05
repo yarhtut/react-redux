2db91c7a967573472fff11d7a7be6a89485703b5
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-09-05 23:00:29 +1200

    add search

--- todo/app/components/TodoApp.jsx
+++ todo/app/components/TodoApp.jsx
@@ -1,10 +1,13 @@
 var React = require ('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
 
 var TodoApp = React.createClass({
   getInitialState: function() {
       return {
+        showCompleted: false,
+        searchText: '',
         todos: [
           {
             id: 1,
@@ -24,15 +27,22 @@ var TodoApp = React.createClass({
         ]
       }
   },
+  handleSearch: function(showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
   handleAddTodo: function (text) {
     alert('new todo: ' + text);
-  
+
   },
   render: function () {
     var {todos} = this.state;
 
     return (
       <div>
+        <TodoSearch onSearch={this.handleSearch}/>
         <TodoList todos={todos}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
